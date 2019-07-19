0a8e72bf50521b1283f7b6d0cd8ec6276b4014ad
Jakub Wolański <jakub.w@mobile-5.com>
2019-07-19 14:05:07 +0200

    update recognition

--- recognize.py
+++ recognize.py
@@ -37,7 +37,7 @@ class Watcher():
             imagedata = np.ndarray(shape=(1, image_height, image_width, channels),
                                    dtype=np.float32)
             try:
-                img = Image.load(file)  # this is a PIL image
+                img = Image.open(file)  # this is a PIL image
             except Exception as e:
                 print(e)
                 print("error loading " + file)
