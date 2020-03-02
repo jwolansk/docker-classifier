f2f8e432b08f86bad0ae03fa1b27a3e53efc185d
kuba <jakub.wolanski@mobile-5.com>
2020-03-02 16:04:47 +0100

    backend swarm config

--- serving/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -d -p 8501:8501 -v /Users/kuba/serving/:/models --mount type=bind,source=/Users/kuba/Documents/home/docker-classifier/models.config,target=/models/models.config --name serving -t serving /usr/bin/tensorflow_model_server --model_config_file=/models/models.config --rest_api_port=8501
