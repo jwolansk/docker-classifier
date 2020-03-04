48086f39b25a553807ee3cc2ab20d564347927b4
kuba <jakub.wolanski@mobile-5.com>
2020-03-05 00:33:26 +0100

    asyncio integration

--- dockerfile
+++ dockerfile
@@ -1,11 +1,11 @@
 FROM ubuntu:latest
 
 RUN apt-get update \
-    && apt-get install -y software-properties-common libssl-dev openssl python3.7
+    && apt-get install -y software-properties-common libssl-dev openssl python3.8
 
 RUN rm /usr/bin/python3
-RUN ln -s /usr/bin/python3.7 /usr/bin/python
-RUN ln -s /usr/bin/python3.7 /usr/bin/python3
+RUN ln -s /usr/bin/python3.8 /usr/bin/python
+RUN ln -s /usr/bin/python3.8 /usr/bin/python3
 
 RUN apt-get install -y python3-pip #python3-dev 
 RUN pip3 install --upgrade pip
@@ -17,5 +17,7 @@ RUN pip3 install h5py
 RUN pip3 install paho-mqtt
 RUN pip3 install watchdog
 RUN pip3 install requests
+RUN pip3 install aiohttp
+RUN pip3 install aiojobs
 
 CMD "/model/run.sh"
--- recognize.py
+++ recognize.py
@@ -3,11 +3,13 @@ import subprocess
 import paho.mqtt.client as mqtt
 import logging
 import json
-import requests
 import time
 import queue
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+import asyncio
+import aiohttp
+import requests
 
 import os.path
 
@@ -18,6 +20,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 hostname = "192.168.1.200"
 CAMERA_NAME = "gate"
+BATCHSIZE = 1
 
 logging.basicConfig(level=logging.INFO)
 handle = "obj-reco"
@@ -25,63 +28,190 @@ logger = logging.getLogger(handle)
 
 image_width = 53
 image_height = 40
-
 channels = 3
+
 
 class Watcher():
     img = Image.new('RGB', (1, 1))
+    pathQueue = queue.Queue()
+    folder = "/data/" + CAMERA_NAME + "/"
+    classes = ['carpassing', 'delivery', 'dodge', 'opel', 'personpassing', 'truck']
+    movement_classes = ['yes', 'no']
+    client = mqtt.Client("docker-classifier-2.0-mbp")
+
+
     def __init__(self):
         self.observer = Observer()
 
-    def run(self, path):
-        q = queue.LifoQueue(10)
-        highQ = queue.LifoQueue(10)
-        event_handler = Handler(q=q, ignore_patterns=['/data/detected.jpg', '/data/' + CAMERA_NAME + '/lastmove.jpg', '*.DS_Store', '*.mp4'])
+    # load train and test dataset
+    def load_data(self, paths):
+
+        imagedata = np.ndarray(shape=(len(paths), image_height, image_width, channels),
+                               dtype=np.float32)
+        for index,filename in enumerate(paths):
+
+            try:
+                img = Image.open(filename)  # this is a PIL image
+            except:
+                return None
+            # img = img.resize((640, 480))
+            ratio = img.size[0] / img.size[1]
+            img = img.resize((int(ratio * image_height), image_height))
+            left = int((ratio * image_height - image_width) / 2)
+            top = 0
+            right = left + image_width
+            bottom = image_height
+
+            img = img.crop((left, top, right, bottom))
+
+            # Convert to Numpy Array
+            x = np.array(img)
+            x = x.reshape((image_height, image_width, 3))
+            # Normalize
+            x = x / 256.0
+            imagedata[index] = x
+
+        return imagedata
+
+    async def handleNewPaths(self, session):
+        logger.info("## creating new path task")
+        while True:
+
+            logger.debug("## path task sleep")
+            path = await self.q.get()
+            logger.debug("#### got q path")
+            paths = [path]
+            data = self.load_data(paths)
+            if data is None:
+                logger.debug("#### none data")
+                continue
+            if not data[0] is None:
+                logger.debug(data[0])
+            else:
+                logger.debug("#### none first data")
+                continue
+
+            logger.debug("## checking " + str(1) + " paths")
+            try:
+                start_time = time.time()
+                servingData = json.dumps(
+                    {"signature_name": "serving_default", "instances": data.tolist()})
+                headers = {"content-type": "application/json"}
+
+                url = 'http://' + hostname + ':8501/v1/models/movement:predict'
+                async with session.post(url, data=servingData, headers=headers) as response:
+                    json_response = await response.json()
+
+                    predictions = json_response['predictions']
+
+                    logger.debug("####### predictions")
+                    logger.debug(predictions)
+                    for index, movement_predictions in enumerate(predictions):
+                        movement_result = np.argmax(movement_predictions)
+                        logString = paths[index] + " %.2f" % (time.time() - start_time) + "s " + self.movement_classes[
+                            movement_result] + ' (' + "%.2f" % movement_predictions[movement_result] + ")"
+
+                        if movement_predictions[movement_result] > 0.75:
+                            if self.movement_classes[movement_result] == 'yes':
+                                element = (paths[index], logString, data[index])
+                                await self.highQ.put(element)
+
+                                subprocess.call("cp '" + paths[index] + "' /data/gate/lastmove.jpg", shell=True)
+                                self.client.publish("gate/object", self.movement_classes[movement_result])
+                                continue
+                        yesnopath = "/data/" + CAMERA_NAME + "/" + self.movement_classes[movement_result]
+                        if not os.path.exists(yesnopath):
+                            subprocess.call('mkdir ' + yesnopath + " &> /dev/null", shell=True)
+
+                        logger.info(logString)
+                        subprocess.call("mv '" + paths[index] + "' " + yesnopath, shell=True)
+
+            except Exception as inst:
+                logger.error(type(inst))  # the exception instance
+                logger.error(inst.args)  # arguments stored in .args
+                logger.error(inst)
+
+
+    async def handleMovementPaths(self, session):
+        logger.info("## creating new move class task")
+        while True:
+            elements = []
+            path = await self.highQ.get()
+            elements.append(path)
+            logger.debug("#### got highq path")
+
+            logger.debug("## checking move " + str(len(elements)) + " paths")
+            data = np.ndarray(shape=(len(elements), image_height, image_width, channels),
+                              dtype=np.float32)
+            for index, image in enumerate(elements):
+                data[index] = image[2]
+
+            try:
+                start_time = time.time()
+                servingData = json.dumps(
+                    {"signature_name": "serving_default", "instances": data.tolist()})
+                headers = {"content-type": "application/json"}
+
+                url = 'http://' + hostname + ':8501/v1/models/objects:predict'
+                async with session.post(url, data=servingData, headers=headers) as response:
+                    json_response = await response.json()
+
+                    predictions = json_response['predictions']
+
+                    logger.debug("####### predictions")
+                    logger.debug(predictions)
+                    for index, movement_predictions in enumerate(predictions):
+                        result = np.argmax(movement_predictions)
+                        logString = elements[index][1] + " - %.2fs ---" % (time.time() - start_time) + " " + self.classes[
+                            result] + ' (' + "%.2f" % predictions[index][result] + ")"
+                        logger.info(logString)
+                        # if predictions[index][result] > 0.45:
+                            # self.client.publish("gate/object", self.classes[result])
+                        classpath = "/data/" + CAMERA_NAME + "/" + self.classes[result]
+                        if not os.path.exists(classpath):
+                            subprocess.call("mkdir " + classpath + " &> /dev/null", shell=True)
+                        subprocess.call("mv '" + elements[index][0] + "' " + classpath, shell=True)
+
+            except Exception as inst:
+                logger.error(type(inst))  # the exception instance
+                logger.error(inst.args)  # arguments stored in .args
+                logger.error(inst)
+            # await asyncio.sleep(0.1)
+
+
+
+    async def producePaths(self):
+        logger.info("## creating new path producer task")
+        try:
+            self.client.loop_start()
+
+            while True:
+
+                logger.debug("sleeping")
+                path = self.pathQueue.get()
+                logger.debug(path)
+                await self.q.put(path)
+                await asyncio.sleep(0.1)
+
+        except KeyboardInterrupt:
+            logger.info("stop")
+        self.observer.join()
+
+    async def run(self, path):
+
+        self.q = asyncio.Queue()
+        self.highQ = asyncio.Queue()
+
+        event_handler = Handler(q=self.pathQueue, ignore_patterns=['/data/detected.jpg', '/data/' + CAMERA_NAME + '/lastmove.jpg', '*.DS_Store', '*.mp4'])
 
         logger.info("--- started")
-        # load train and test dataset
-        def load_data(paths):
-
-            imagedata = np.ndarray(shape=(len(paths), image_height, image_width, channels),
-                                   dtype=np.float32)
-            for index,filename in enumerate(paths):
-
-                try:
-                    img = Image.open(filename)  # this is a PIL image
-                except:
-                    return None
-                # img = img.resize((640, 480))
-                ratio = img.size[0] / img.size[1]
-                img = img.resize((int(ratio * image_height), image_height))
-                left = int((ratio * image_height - image_width) / 2)
-                top = 0
-                right = left + image_width
-                bottom = image_height
-
-                img = img.crop((left, top, right, bottom))
-
-                # Convert to Numpy Array
-                x = np.array(img)
-                x = x.reshape((image_height, image_width, 3))
-                # Normalize
-                x = x / 256.0
-                imagedata[index] = x
-
-            return imagedata
-
-        # load model
-        folder = "/data/" + CAMERA_NAME + "/"
-
-        classes = ['carpassing', 'delivery', 'dodge', 'opel', 'personpassing', 'truck']
-        movement_classes = ['yes', 'no']
 
         self.observer.schedule(event_handler, path, recursive=True)
         self.observer.start()
         logger.info("handler started")
 
-        client = mqtt.Client("docker-classifier-2.0-mbp")
-        client.connect("192.168.1.253", 1883, 60)
-        client.loop_start()
+        self.client.connect("192.168.1.253", 1883, 60)
+        self.client.loop_start()
 
         def on_disconnect(client, userdata, rc):
             if rc != 0:
@@ -90,112 +220,15 @@ class Watcher():
         def on_connect(client, userdata, rc):
             logger.info("MQTT Client Connected")
 
-        client.on_disconnect = on_disconnect
-        client.on_connect = on_connect
+        self.client.on_disconnect = on_disconnect
+        self.client.on_connect = on_connect
 
-        try:
-            client.loop_start()
+        async with aiohttp.ClientSession() as session:
+            self.tasks = [asyncio.create_task(self.handleNewPaths(session)) for _ in range(5)] +\
+                         [asyncio.create_task(self.handleMovementPaths(session)) for _ in range(4)] +\
+                         [asyncio.create_task(self.producePaths())]
 
-            while True:
-                if q.empty() and highQ.empty():
-                    time.sleep(0.5)
-                    continue
-
-                if not q.empty():
-                    time.sleep(0.1)
-                    pathSet = set([])
-                    while not q.empty():
-                        path = q.get(False)
-                        pathSet.add(path)
-
-                    paths = list(pathSet)
-                    data = load_data(paths)
-                    if data is None:
-                        continue
-                    if not data[0] is None:
-                        logger.debug(data[0])
-                    else:
-                        continue
-
-                    try:
-                        start_time = time.time()
-                        servingData = json.dumps(
-                            {"signature_name": "serving_default", "instances": data.tolist()})
-                        headers = {"content-type": "application/json"}
-                        json_response = requests.post('http://' + hostname + ':8501/v1/models/movement:predict',
-                                                      data=servingData, headers=headers)
-                        predictions = json.loads(json_response.text)['predictions']
-
-                        logger.debug("####### predictions")
-                        logger.debug(predictions)
-                        for index, movement_predictions in enumerate(predictions):
-                            movement_result = np.argmax(movement_predictions)
-                            logString = paths[index] + " %.2f" % (time.time() - start_time) + "s " + movement_classes[
-                                movement_result] + ' (' + "%.2f" % movement_predictions[movement_result] + ")"
-
-                            if movement_predictions[movement_result] > 0.75:
-                                if movement_classes[movement_result] == 'yes':
-                                    element = (paths[index], logString, data[index])
-                                    highQ.put(element)
-
-                                    subprocess.call("cp '" + paths[index] + "' /data/gate/lastmove.jpg", shell=True)
-                                    client.publish("gate/object", movement_classes[movement_result])
-                                    continue
-                            yesnopath = "/data/" + CAMERA_NAME + "/" + movement_classes[movement_result]
-                            if not os.path.exists(yesnopath):
-                                subprocess.call('mkdir ' + yesnopath + " &> /dev/null", shell=True)
-
-                            logger.info(logString)
-                            subprocess.call("mv '" + paths[index] + "' " + yesnopath, shell=True)
-
-                    except Exception as inst:
-                        logger.error(type(inst))  # the exception instance
-                        logger.error(inst.args)  # arguments stored in .args
-                        logger.error(inst)
-
-                if not highQ.empty():
-                    elements = []
-                    while not highQ.empty():
-                        element = highQ.get(False)
-                        elements.append(element)
-
-                    logger.debug(elements)
-                    data = np.ndarray(shape=(len(elements), image_height, image_width, channels),
-                                           dtype=np.float32)
-                    for index, image in enumerate(elements):
-                        data[index] = image[2]
-
-                    try:
-                        start_time = time.time()
-                        servingData = json.dumps(
-                            {"signature_name": "serving_default", "instances": data.tolist()})
-                        headers = {"content-type": "application/json"}
-                        json_response = requests.post('http://' + hostname + ':8501/v1/models/objects:predict',
-                                                      data=servingData, headers=headers)
-                        predictions = json.loads(json_response.text)['predictions']
-
-                        logger.debug("####### predictions")
-                        logger.debug(predictions)
-                        for index, movement_predictions in enumerate(predictions):
-                            result = np.argmax(movement_predictions)
-                            logString = elements[index][1] + " - %.2fs ---" % (time.time() - start_time) + " " + classes[
-                                result] + ' (' + "%.2f" % predictions[index][result] + ")"
-                            logger.info(logString)
-                            if predictions[index][result] > 0.45:
-                                client.publish("gate/object", classes[result])
-                            classpath = "/data/" + CAMERA_NAME + "/" + classes[result]
-                            if not os.path.exists(classpath):
-                                subprocess.call("mkdir " + classpath + " &> /dev/null", shell=True)
-                            subprocess.call("mv '" + elements[index][0] + "' " + classpath, shell=True)
-
-                    except Exception as inst:
-                        logger.error(type(inst))  # the exception instance
-                        logger.error(inst.args)  # arguments stored in .args
-                        logger.error(inst)
-
-        except KeyboardInterrupt:
-            logger.info("stop")
-        self.observer.join()
+            await asyncio.gather(*self.tasks)
 
 
 class Handler(PatternMatchingEventHandler):
@@ -213,12 +246,14 @@ class Handler(PatternMatchingEventHandler):
 
         # Take any action here when a file is first created.
         path = "%s" % event.src_path
-        # print(path)
         self.q.put(path)
 
 
-if __name__ == '__main__':
+async def main():
 
     w = Watcher()
-    w.run("/data/" + CAMERA_NAME + "/")
+    await w.run("/data/" + CAMERA_NAME + "/")
+
+
+asyncio.run(main())
 
